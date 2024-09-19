3ffc7bc262de8dd2e39453cb93c53837f54b55e3
Muktar Sadiq <muktarsadiq.m@yahoo.com>
2024-09-19 20:41:17 +0100

    get request commit

---
+++ target/debug/deps/Get_Request-f9dff7454b3cd276.d
@@ -0,0 +1,5 @@
+/Users/user/Desktop/rust_50/get_request/target/debug/deps/Get_Request-f9dff7454b3cd276: src/main.rs
+
+/Users/user/Desktop/rust_50/get_request/target/debug/deps/Get_Request-f9dff7454b3cd276.d: src/main.rs
+
+src/main.rs:
